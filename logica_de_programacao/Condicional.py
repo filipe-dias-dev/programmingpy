d1a0806de9013fc02c52b9b23f53236c93db4578
filipe <filipe.dias09@hotmail.com>
2022-11-04 21:43:10 -0300

    condicional

---
+++ logica_de_programacao/Condicional.py
@@ -0,0 +1,9 @@
+idade = 12
+
+def verifica_se_pode_dirigir(idade):
+    if idade >= 18:
+      print('Tem permissão para dirigi!')
+    else:
+      print('Não tem permissão para dirigir!')
+
+verifica_se_pode_dirigir(idade)
