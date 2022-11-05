cbf1bc3f9b1f2fbdab5ba40a6ce6214680418e71
filipe <filipe.dias09@hotmail.com>
2022-11-04 21:53:59 -0300

    type list

--- logica_de_programacao/lista.py
+++ logica_de_programacao/lista.py
@@ -7,3 +7,8 @@ print(idades[0:3])
 print(idades[1:])
 print(idades[-1])
 print(idades[-2])
+
+lista = ['Filipe', 28, True, '18']
+
+for elemento in lista:
+  print(f'O elemento {elemento} é do tipo, ', type(elemento))
